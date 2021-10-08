018b29329b3d0b99c96ba311dee749ca9e102e3f
Bram <84628826+sys-256@users.noreply.github.com>
2021-10-08 17:37:34 +0200

    yes

--- src/init.cpp
+++ src/init.cpp
@@ -558,7 +558,7 @@ std::string LicenseInfo()
     const std::string URL_SOURCE_CODE = "<https://github.com/sys-256/sircoin>";
     const std::string URL_WEBSITE = "<https://sircoin.tk>";
 
-    return CopyrightHolders(strprintf(_("Copyright (C) %i-%i"), 2011, COPYRIGHT_YEAR) + " ") + "\n" +
+    return CopyrightHolders(strprintf(_("Copyright (C) %i-%i"), 2021, COPYRIGHT_YEAR) + " ") + "\n" +
            "\n" +
            strprintf(_("Please contribute if you find %s useful. "
                        "Visit %s for further information about the software."),
